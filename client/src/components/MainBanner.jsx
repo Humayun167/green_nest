098ad65c6d9668baf111557145dcc6f8ca53c655
Md Humayun Rashid Khan <humayunrashidkhan96@gmail.com>
2025-05-22 01:52:25 +0600

    main banner and categories

--- client/src/components/MainBanner.jsx
+++ client/src/components/MainBanner.jsx
@@ -1,13 +1,47 @@
-import React from 'react';
-import { assets } from '../assets/assets';
+import { assets } from "../assets/assets";
+import { Link } from "react-router-dom";
 
 const MainBanner = () => {
-    return (
-        <div>
-            <img className='w-full hidden md:block' src={assets.main_banner_bg} alt="" />
-            <img className='w-full md:hidden' src={assets.main_banner_bg_sm} alt="" />
+  return (
+    <div className="relative">
+      <img
+        className="w-full hidden md:block"
+        src={assets.main_banner_bg}
+        alt=""
+      />
+      <img className="w-full md:hidden" src={assets.main_banner_bg_sm} alt="" />
+      <div className="absolute inset-0 flex flex-col items-center md:items-start justify-end md:justify-center pb-24 md:pl-18 lg:pl-24">
+        <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold md:text-left max-w-72 md:max-w-80 lg:max-w-105 leading-tight lg:leading-15">
+         Discover, Grow, and Share Plants with Nature Lovers
+        </h1>
+
+        <div className="flex items-center mt-6 font-medium">
+          <Link
+            to={"/products"}
+            className="group flex items-center gap-2 px-7 md:px-9 py-3 bg-primary hover:bg-primary-dull transition rounded text-white cursor-pointer"
+          >
+            Shop now
+            <img
+              className="md:hidden transition group-focus:translate-x-1"
+              src={assets.white_arrow_icon}
+              alt="arrow"
+            />
+          </Link>
+          <Link
+            to={"/products"}
+            className="group hidden md:flex items-center gap-2 px-9 cursor-pointer"
+          >
+            Explore deals
+            <img
+              className="transition group-hover:transition-x-1"
+              src={assets.black_arrow_icon}
+              alt="arrow"
+            />
+          </Link>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default MainBanner;
