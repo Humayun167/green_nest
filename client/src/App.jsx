91237856594891ee97292c9d3e0261bf4445effa
Md Humayun Rashid Khan <humayunrashidkhan96@gmail.com>
2025-05-23 23:33:19 +0600

    login page done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import { Route, Routes, useLocation } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import Footer from './components/Footer';
 import { useAppContext } from './context/AppContext';
-import LoginPage from './components/LoginPage';
+import Login from './components/Login';
 
 const App = () => {
 
@@ -15,7 +14,7 @@ const App = () => {
   return (
     <div>
       {isSellerPath? null: <Navbar/>}
-      {showUserLogin ? <LoginPage/>: null}
+      {showUserLogin ? <Login/>: null}
       <Toaster/>
 
        <div className={`${isSellerPath ?"":"px-6 md:px-16 lg:px-24 xl:px-32" }`}>
